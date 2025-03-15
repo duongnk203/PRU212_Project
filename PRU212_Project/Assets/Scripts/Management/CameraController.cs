0b85b69c456b585a7c415d85f4adf4f6087e8011
Dung Le Duc <DungLDHE173255@fpt.edu.vn>
2025-03-15 12:49:47 +0700

    Part32 | Enemies + Ghost

--- PRU212_Project/Assets/Scripts/Management/CameraController.cs
+++ PRU212_Project/Assets/Scripts/Management/CameraController.cs
@@ -7,6 +7,11 @@ public class CameraController : Singleton<CameraController>
 {
     private CinemachineCamera cinemachineVirtualCamera;
 
+    private void Start()
+    {
+        SetPlayerCameraFollow();
+    }
+
     public void SetPlayerCameraFollow()
     {
         cinemachineVirtualCamera = FindObjectOfType<CinemachineCamera>();
